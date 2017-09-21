262331c798d3d5e572ea851613df8790a3ee69c5
ronttonen <roni.ahti@edu.turkuamk.fi>
2017-09-21 10:48:24 +0300

    Lisätty tiedostot rakenteeseen ja alku html

---
+++ ilmoitustaulu/views.py
@@ -0,0 +1,6 @@
+from ilmoitustaulu import app
+from flask import render_template
+
+@app.route('/')
+def index():
+	return render_template('home.html')
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name='ilmoitustaulu',
+	packages=['ilmoitustaulu'],
+	include_package_data=True,
+	install_requires=[
+		'flask',
+	],
+)
