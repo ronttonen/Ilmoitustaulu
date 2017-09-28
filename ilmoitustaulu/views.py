63f9a7e590bf80d9fd50d820d486f3a10e5d7864
jopol <joel.perasto@gmail.com>
2017-09-28 15:46:17 +0300

    hakkeroin

--- ilmoitustaulu/views.py
+++ ilmoitustaulu/views.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route('/')
 def index():
 	return render_template('home.html')
+
+@app.route("/joel")
+def joelsivu():
+	return render_template("joelsivu.html")
