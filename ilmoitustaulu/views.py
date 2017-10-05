65b358026172070436f987a7c46fb3efd1712e31
ronttonen <roni.ahti@edu.turkuamk.fi>
2017-10-05 10:33:22 +0300

    lisätty reittejä, pitää asentaa flask login eli 'pip install flask-login'

--- ilmoitustaulu/views.py
+++ ilmoitustaulu/views.py
@@ -8,3 +8,19 @@ def index():
 @app.route("/joel")
 def joelsivu():
 	return render_template("joelsivu.html")
+
+@app.route('/login')
+def login():
+	return render_template("joelsivu.html")
+
+@app.route('/logout')
+def logout():
+	return render_template("joelsivu.html")
+
+@app.route('/create_event')
+def create_event():
+	return render_template("joelsivu.html")
+
+@app.route('/list_events')
+def list_events():
+	return render_template("joelsivu.html")
